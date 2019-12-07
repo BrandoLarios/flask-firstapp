d9b164b49fcbfe3bd4097f8cda52544cb25ebd5d
BrandoLarios <brandolarios@outlook.com>
2019-12-06 19:53:48 -0600

    Graficas añadidas (No muestra lineas y barras)

--- graficator.py
+++ graficator.py
@@ -60,71 +60,102 @@ def page_not_found(error):
 
 def line_graphic(column, df, title): #Grafica de lineas
     if column == 'Year':
-        #Definir la creacion de la grafica de lineas para la columna Year
+        plt.plot(df.loc[490:510, 'Year'])
+        plt.title('Year')
+        print('Barras 1')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Rank':
-        #Definir la creacion de la grafica de lineas para la columna Rank
+        plt.plot(df.loc[0:800, 'Rank'])
+        print('Barras 2')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Company':
         #Definir la creacion de la grafica de lineas para la columna Company
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Revenue (in millions)':
-        #Definir la creacion de la grafica de lineas para la columna Revenue (in millions)
-        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
-    elif column == 'Profit (in millions)':
-        #Definir la creacion de la grafica de lineas para la columna Profit (in millions)
+        plt.plot(df.loc[490:550, 'Revenue (in millions)'])
+        print('Barras 3')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
 
+    elif column == 'Profit (in millions)':
+        plt.plot(df.loc[490:510, 'Profit (in millions)'])
+        print('Barras 4')
+        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+        
 def bar_graphic(column, df, title): #Grafica de barras
     if column == 'Year':
-            #Definir la creacion de la grafica de barras para la columna Year
+        ingresos = pd.DataFrame((df['Revenue (in millions)']).groupby(df['Company']).mean())
+        lista_ingresos =ingresos.index
+        lista_ingresos=lista_ingresos[0:5]
+        ingresos=ingresos[0:5]
+        plt.bar(lista_ingresos,ingresos['Revenue (in millions)'].values) 
+        plt.title('Promedio de ingresos por compañia de 1955 a 2005')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Rank':
-        #Definir la creacion de la grafica de barras para la columna Rank
+        frecuencia =  pd.DataFrame((df['Company']).groupby(df['Company']).count())
+        lista = frecuencia.index
+        lista = lista[0:5]
+        frecuencia = frecuencia[0:5]
+        plt.bar(lista,frecuencia['Company'].values) 
+        plt.title('Aparicion en  el ranking por compañia') 
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Company':
         #Definir la creacion de la grafica de barras para la columna Company
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Revenue (in millions)':
-        #Definir la creacion de la grafica de barras para la columna Revenue (in millions)
+        plt.bar(df.loc[0:5,'Company'],df.loc[0:5, column]) 
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Profit (in millions)':
-        #Definir la creacion de la grafica de barras para la columna Profit (in millions)
+        plt.bar(df.loc[0:5,'Company'],df.loc[0:5, column])   
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
 
 def pie_graphic(column, df, title): #Grafica de pastel
     if column == 'Year':
-            #Definir la creacion de la grafica de pastel para la columna Year
+        plt.pie(df['Year'][0:13], labels=df['Company'][0:13], autopct='%0.1f%%')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Rank':
-        #Definir la creacion de la grafica de pastel para la columna Rank
+        plt.pie(df['Rank'][0:9], labels=df['Company'][0:9], autopct='%0.1f%%')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Company':
         #Definir la creacion de la grafica de pastel para la columna Company
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
     elif column == 'Revenue (in millions)':
-        #Definir la creacion de la grafica de pastel para la columna Revenue (in millions)
-        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
-    elif column == 'Profit (in millions)':
-        #Definir la creacion de la grafica de pastel para la columna Profit (in millions)
+        plt.pie(df['Revenue (in millions)'][0:9], labels=df['Company'][0:9], autopct='%0.1f%%')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
 
+    elif column == 'Profit (in millions)':
+        plt.pie(df['Profit (in millions)'][0:5], labels=df['Company'][0:5], autopct='%0.1f%%')
+        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+        
 def point_graphic(column, df, title): #Grafica de puntos
     if column == 'Year':
-            #Definir la creacion de la grafica de puntos para la columna Year
-        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
-    elif column == 'Rank':
-        #Definir la creacion de la grafica de puntos para la columna Rank
-        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
-    elif column == 'Company':
-        #Definir la creacion de la grafica de puntos para la columna Company
-        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
-    elif column == 'Revenue (in millions)':
-        #Definir la creacion de la grafica de puntos para la columna Revenue (in millions)
-        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
-    elif column == 'Profit (in millions)':
-        #Definir la creacion de la grafica de puntos para la columna Profit (in millions)
+        plt.plot(df.loc[490:510, 'Year'], '--b')
         plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
 
+    elif column == 'Rank':
+        plt.plot(df.loc[0:800, 'Rank'], '--b')
+        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
+    elif column == 'Company':
+        #Definir la creacion de la grafica de lineas para la columna Company
+        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
+    elif column == 'Revenue (in millions)':
+        plt.plot(df.loc[490:550, 'Revenue (in millions)'], '--b')
+        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+
+    elif column == 'Profit (in millions)':
+        plt.plot(df.loc[490:510, 'Profit (in millions)'], '--b')
+        plt.savefig('static/'+str(title)+'.png') #Guarda la grafica con el nombre dado
+        
 if __name__ == '__main__':
     app.run(debug=True)
